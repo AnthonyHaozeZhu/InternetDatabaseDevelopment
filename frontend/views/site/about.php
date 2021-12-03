63774f3e2c60201298608889f94e75425cee1205
AnthonyZ <1119886232@qq.com>
2021-12-03 23:59:28 +0800

    嵌入了about

--- frontend/views/site/about.php
+++ frontend/views/site/about.php
@@ -4,13 +4,197 @@
 
 use yii\helpers\Html;
 
-$this->title = 'About';
+$this->title = '关于我们';
 $this->params['breadcrumbs'][] = $this->title;
+//$this->context->layout = false; //不使用布局
 ?>
-<div class="site-about">
-    <h1><?= Html::encode($this->title) ?></h1>
 
-    <p>This is the About page. You may modify the following file to customize its content:</p>
+<div class="site-section bg-image overlay">
+  <div class="container">
+	<div class="row justify-content-center mb-5">
+	  <div class="col-md-7 text-center border-primary">
+		<h2 class="font-weight-light text-primary" data-aos="fade">个人作业</h2>
+	  </div>
+	</div>
+	<div class="row">
+	  <div class="col-md-6 col-lg-4 mb-5 mb-lg-0" data-aos="fade" data-aos-delay="100">
+		<div class="how-it-work-item">
+		  <span class="number">1</span>
+		  <div class="how-it-work-body">
+			<h2>朱浩泽 1911530</h2>
+			<ul class="ul-check list-unstyled success">
+			  <li class="text-white">data/personal/作业1(1911530_朱浩泽)</li>
+			  <li class="text-white">data/personal/作业2(1911530_朱浩泽)</li>
+			  <li class="text-white">data/personal/作业3(1911530_朱浩泽)</li>
+			</ul>
+		  </div>
+		</div>
+	  </div>
 
-    <code><?= __FILE__ ?></code>
+	  <div class="col-md-6 col-lg-4 mb-5 mb-lg-0" data-aos="fade" data-aos-delay="200">
+		<div class="how-it-work-item">
+		  <span class="number">2</span>
+		  <div class="how-it-work-body">
+			<h2>宣恩允 1911501</h2>
+			<ul class="ul-check list-unstyled success">
+              <li class="text-white">data/personal/作业1(1911501_宣恩允)</li>
+			  <li class="text-white">data/personal/作业2(1911501_宣恩允)</li>
+			  <li class="text-white">data/personal/作业3(1911501_宣恩允)</li>
+			</ul>
+		  </div>
+		</div>
+	  </div>
+
+	  <div class="col-md-6 col-lg-4 mb-5 mb-lg-0" data-aos="fade" data-aos-delay="300">
+		<div class="how-it-work-item">
+		  <span class="number">3</span>
+		  <div class="how-it-work-body">
+			<h2>胡皓冬 1911419</h2>
+			<ul class="ul-check list-unstyled success">
+              <li class="text-white">data/personal/作业1(1911419_胡皓冬)</li>
+			  <li class="text-white">data/personal/作业2(1911419_胡皓冬)</li>
+			  <li class="text-white">data/personal/作业3(1911419_胡皓冬)</li>
+			</ul>
+		  </div>
+		</div>
+	  </div>
+
+	  <div class="col-md-6 col-lg-4 mb-5 mb-lg-0" data-aos="fade" data-aos-delay="400">
+		<div class="how-it-work-item">
+		  <span class="number">4</span>
+		  <div class="how-it-work-body">
+			<h2>尧泽斌 1911506</h2>
+			<ul class="ul-check list-unstyled success">
+              <li class="text-white">data/personal/作业1(1911506_尧泽斌)</li>
+			  <li class="text-white">data/personal/作业2(1911506_尧泽斌)</li>
+			  <li class="text-white">data/personal/作业3(1911506_尧泽斌)</li>
+			</ul>
+		  </div>
+		</div>
+	  </div>
+
+	</div>
+  </div>
+</div>
+
+<div class="site-section bg-image overlay">
+  <div class="container">
+	<div class="row justify-content-center mb-5">
+	  <div class="col-md-7 text-center border-primary">
+		<h2 class="font-weight-light text-primary" data-aos="fade">团队作业</h2>
+	  </div>
+	</div>
+	<div class="row">
+	  <div class="col-md-6 col-lg-4 mb-5 mb-lg-0" data-aos="fade" data-aos-delay="100">
+		<div class="how-it-work-item">
+		  <span class="number">1</span>
+		  <div class="how-it-work-body">
+			<h2>团队作业1——需求文档</h2>
+			<ul class="ul-check list-unstyled success">
+			  <li class="text-white">data/team/指数小队_需求文档(1911530_1911501_1911506_1911419).pdf</li>
+			</ul>
+		  </div>
+		</div>
+	  </div>
+
+      <div class="col-md-6 col-lg-4 mb-5 mb-lg-0" data-aos="fade" data-aos-delay="100">
+		<div class="how-it-work-item">
+		  <span class="number">2</span>
+		  <div class="how-it-work-body">
+			<h2>团队作业2——设计文档</h2>
+			<ul class="ul-check list-unstyled success">
+			  <li class="text-white">data/team/指数小队_设计文档(1911530_1911501_1911506_1911419).pdfr</li>
+			</ul>
+		  </div>
+		</div>
+	  </div>
+
+
+      <div class="col-md-6 col-lg-4 mb-5 mb-lg-0" data-aos="fade" data-aos-delay="100">
+		<div class="how-it-work-item">
+		  <span class="number">3</span>
+		  <div class="how-it-work-body">
+			<h2>团队作业3——实现文档</h2>
+			<ul class="ul-check list-unstyled success">
+			  <li class="text-white">data/team/指数小队_实现文档(1911530_1911501_1911506_1911419).pdf</li>
+			</ul>
+		  </div>
+		</div>
+	  </div>
+
+      <div class="col-md-6 col-lg-4 mb-5 mb-lg-0" data-aos="fade" data-aos-delay="100">
+		<div class="how-it-work-item">
+		  <span class="number">4</span>
+		  <div class="how-it-work-body">
+			<h2>团队作业4——用户手册</h2>
+			<ul class="ul-check list-unstyled success">
+			  <li class="text-white">data/team/指数小队_用户手册(1911530_1911501_1911506_1911419).pdf</li>
+			</ul>
+		  </div>
+		</div>
+	  </div>
+
+      <div class="col-md-6 col-lg-4 mb-5 mb-lg-0" data-aos="fade" data-aos-delay="100">
+		<div class="how-it-work-item">
+		  <span class="number">5</span>
+		  <div class="how-it-work-body">
+			<h2>团队作业5——部署文档</h2>
+			<ul class="ul-check list-unstyled success">
+			  <li class="text-white">data/team/指数小队_部署文档(1911530_1911501_1911506_1911419).pdf</li>
+			</ul>
+		  </div>
+		</div>
+	  </div>
+
+      <div class="col-md-6 col-lg-4 mb-5 mb-lg-0" data-aos="fade" data-aos-delay="100">
+		<div class="how-it-work-item">
+		  <span class="number">6</span>
+		  <div class="how-it-work-body">
+			<h2>团队作业6——项目展示</h2>
+			<ul class="ul-check list-unstyled success">
+			  <li class="text-white">data/team/指数小队_项目展示(1911530_1911501_1911506_1911419).pdf</li>
+			</ul>
+		  </div>
+		</div>
+	  </div>
+
+      <div class="col-md-6 col-lg-4 mb-5 mb-lg-0" data-aos="fade" data-aos-delay="100">
+		<div class="how-it-work-item">
+		  <span class="number">7</span>
+		  <div class="how-it-work-body">
+			<h2>团队作业7——录屏ppt讲解</h2>
+			<ul class="ul-check list-unstyled success">
+			  <li class="text-white">data/team/指数小队_录屏ppt讲解(1911530_1911501_1911506_1911419).pdf</li>
+			</ul>
+		  </div>
+		</div>
+	  </div>
+
+      <div class="col-md-6 col-lg-4 mb-5 mb-lg-0" data-aos="fade" data-aos-delay="100">
+		<div class="how-it-work-item">
+		  <span class="number">8</span>
+		  <div class="how-it-work-body">
+			<h2>团队作业7——录屏文件部署</h2>
+			<ul class="ul-check list-unstyled success">
+			  <li class="text-white">data/team/指数小队_录屏文件部署(1911530_1911501_1911506_1911419).pdf</li>
+			</ul>
+		  </div>
+		</div>
+	  </div>
+
+      <div class="col-md-6 col-lg-4 mb-5 mb-lg-0" data-aos="fade" data-aos-delay="100">
+		<div class="how-it-work-item">
+		  <span class="number">9</span>
+		  <div class="how-it-work-body">
+			<h2>团队作业7——录屏网页讲解</h2>
+			<ul class="ul-check list-unstyled success">
+			  <li class="text-white">data/team/指数小队_录屏网页讲解(1911530_1911501_1911506_1911419).pdf</li>
+			</ul>
+		  </div>
+		</div>
+	  </div>
+
+	</div>
+  </div>
+
 </div>
